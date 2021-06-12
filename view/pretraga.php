4d6e3a95465b198d74ba48a96c82b57dc25af12e
HP 830 G5 <pavlerakovic1@gmail.com>
2021-06-12 13:07:52 +0200

    Commited again

--- view/pretraga.php
+++ view/pretraga.php
@@ -22,9 +22,6 @@
 	
 <body>
 	<div id="wrap">
-		<div id="header">
-			<img class="hederi" src="../img/header1.jpg">
-		</div>
 		<div id="meni">	
 			<ul> 
 				<li><a href="../home.php">Početna</a></li> 
